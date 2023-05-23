627ed259967fdb361b7496b64c94e2dd451669eb
AjdiNNN <ajdinhukic007@gmail.com>
2023-05-23 04:56:30 +0200

    Pyhton cleanup

--- logger.py
+++ logger.py
@@ -5,19 +5,17 @@ import psutil
 import platform
 import pygetwindow as gw
 import requests
-import os
 import asyncio
 import atexit
 
 session = {"sessionid": 0, "start": None, "end": None}
 
 def update():
-    print("a")
-    r = requests.put(URL+"/session/"+ session['sessionid'], json={"end": str(datetime.now())}, headers={"Authorization": passcode})
+    requests.put(URL+"/session/"+ session['sessionid'], json={"end": str(datetime.now())}, headers={"Authorization": passcode})
 atexit.register(update)
 
-URL = os.getenv('api', 'http://localhost/keytrack-edu/api')
-passcode = os.getenv('passcode', 'PyhtonKey-edu!')
+URL = 'http://localhost/keytrack-edu/api'
+passcode = 'PyhtonKey-edu!'
 
 def is_vscode_running():
     if platform.system() == "Windows":
@@ -38,14 +36,14 @@ def on_press(key):
                 try:
                     code = int(''.join(f'{ord(c)}' for c in key.char))
                     data = {"pressed": key.char if code > 32 else code, "pressedAt": str(datetime.now()), "special": False, "session_id": session['sessionid']}
-                    r = requests.post(URL+"/keyboard", json=data, headers={"Authorization": passcode})
+                    requests.post(URL+"/keyboard", json=data, headers={"Authorization": passcode})
                 except AttributeError:
                     data = {"pressed":  str(key), "pressedAt": str(datetime.now()), "special": True, "session_id": session['sessionid']}
-                    r = requests.post(URL+"/keyboard", json=data, headers={"Authorization": passcode})
+                    requests.post(URL+"/keyboard", json=data, headers={"Authorization": passcode})
         except gw.PyGetWindowException:
             pass
     else:
-        r = requests.put(URL+"/session/"+ session['sessionid'], json={"end": str(datetime.now())}, headers={"Authorization": passcode})
+        requests.put(URL+"/session/"+ session['sessionid'], json={"end": str(datetime.now())}, headers={"Authorization": passcode})
         asyncio.run(main())
 
 def on_click(x, y, button, pressed):
@@ -54,17 +52,17 @@ def on_click(x, y, button, pressed):
             active_window = gw.getActiveWindow()
             if active_window is not None and "Visual Studio Code" in active_window.title and active_window.isMaximized:
                     data = {"x": x,"y": y, "pressedAt": str(datetime.now()), "isRight": button is button.right, "released": pressed is False,"session_id": session['sessionid']}
-                    r = requests.post(URL+"/mouse", json=data, headers={"Authorization": passcode})
+                    requests.post(URL+"/mouse", json=data, headers={"Authorization": passcode})
         except gw.PyGetWindowException:
             pass
     else:
-        r = requests.put(URL+"/session/"+ session['sessionid'], json={"end": str(datetime.now())}, headers={"Authorization": passcode})
+        requests.put(URL+"/session/"+ session['sessionid'], json={"end": str(datetime.now())}, headers={"Authorization": passcode})
         asyncio.run(main())
 
 async def main():
     while is_vscode_running is False:
         await asyncio.sleep(1)
-    r = requests.post(URL+"/session", json={"start": str(datetime.now()), "end": str(datetime.now())}, headers={"Authorization": passcode})
+    requests.post(URL+"/session", json={"start": str(datetime.now()), "end": str(datetime.now())}, headers={"Authorization": passcode})
     global session
     session = {"sessionid": r.json()['id'], "start": r.json()['id'], "end": None}
 
@@ -76,7 +74,6 @@ async def main():
     mouse_listener.start()
     keyboard_listener.join()
     mouse_listener.join()
-
 
 asyncio.run(main())
 
