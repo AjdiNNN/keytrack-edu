044a759b1e20358d3cc56e7ba81a713fa364bfd9
AjdiNNN <ajdinhukic007@gmail.com>
2023-05-25 23:20:25 +0200

    Testing

--- api/index.php
+++ api/index.php
@@ -27,8 +27,9 @@ return urldecode($query_param);
 
 Flight::route('/*', function(){
   $headers = getallheaders();
-  Flight::json(["test" => $headers['Authorization'], "test" => Config::PASSCODE()]);
   if (@!$headers['Authorization'] || $headers['Authorization'] != Config::PASSCODE()){
+    Flight::json(["message" => $headers], 403);
+    Flight::json(["message" => $headers['Authorization']], 403);
     Flight::json(["message" => "Authorization is missing or its wrong one"], 403);
     return FALSE;
   }else{
