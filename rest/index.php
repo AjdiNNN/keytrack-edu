5d285af491514da033762bcd830b42c5e5f6c8ba
AjdiNNN <ajdinhukic007@gmail.com>
2023-06-19 03:09:23 +0200

    Cleanup

--- rest/index.php
+++ rest/index.php
@@ -53,21 +53,6 @@ Flight::route('/*', function(){
     }
 });
 
-Flight::route('POST /login', function(){
-    $login = Flight::request()->data->getData();
-    $user = Flight::userDao()->get_user_by_username($login['username']);
-    if (isset($user['id'])){
-      if(password_verify($login['password'],$user['password'])){
-        unset($user['password']);
-        $jwt = JWT::encode($user, Config::JWT_SECRET(), 'HS256');
-        Flight::json(['token' => $jwt]);
-      }else{
-        Flight::json(["message" => "Wrong password"], 404);
-      }
-    }else{
-      Flight::json(["message" => "User doesn't exist"], 405);
-    }
-});
 // Include route files
 require_once __DIR__.'/routes/SessionRoutes.php';
 require_once __DIR__.'/routes/KeyboardRoutes.php';
